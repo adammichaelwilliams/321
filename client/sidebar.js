62d2b78f4dffe417eab6c7bb2303f35a905513a7
Shane Reustle <me@shanereustle.com>
2015-10-10 15:51:23 +0900

    collections list

---
+++ client/sidebar.js
@@ -0,0 +1,18 @@
+Template.sidebar.helpers({
+	collections: function(){
+		
+		// TODO
+		return [{
+			_id: 'asdf123',
+			name: 'renters'
+		},{
+			_id: 'asdf124',
+			name: 'houses'
+		},{
+			_id: 'asdf125',
+			name: 'brokerages'
+		}];
+		
+	}
+});
+
