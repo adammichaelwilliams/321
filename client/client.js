c6f947b9c5ed293f2c32650f1bcbc9bea3bbb822
Shane Reustle <me@shanereustle.com>
2015-10-10 16:52:53 +0900

    documents page roughly

--- client/client.js
+++ client/client.js
@@ -1,15 +1,35 @@
 
 Template.collectionDetail.helpers({
+	
 	collection: function(){
 		// Todo the whole thing
 		return FlowRouter.getParam('collectionName');
 	}
+	
 });
 
 Template.collectionDocs.helpers({
+	
 	collection: function(){
 		// Todo the whole thing
 		return FlowRouter.getParam('collectionName');
+	},
+	
+	collectionDocs: function(){
+		return [{
+			foo: 123,
+			bar: 234,
+			baz: 414
+		},{
+			foo: 123,
+			bar: 234,
+			baz: 414
+		},{
+			foo: 123,
+			bar: 234,
+			baz: 99999
+		}];
 	}
+	
 });
 
