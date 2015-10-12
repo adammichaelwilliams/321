4a3b89437f010ce0223fc67f2d2845869ab8f370
Shane Reustle <me@shanereustle.com>
2015-10-12 22:07:21 +0900

    use flat style

--- client/client.js
+++ client/client.js
@@ -49,50 +49,32 @@ Template.collectionDetail.helpers({
 
 	fields: function(){
 		
-		var dummyData = [
-			{
-				'key': 'propertyType',
-				'types': {
-					'string': 9,
-					'integer': 49,
-					'object': 19,
-					'not-found': 4,
-					'total': 104
-				},
-				'children': [
-					{
-						'key': 'houseType',
-						'types': {
-							'string': 94,
-							'not-found': 4,
-							'total': 95
-						}
-					},
-					{
-						'key': 'acreage',
-						'types': {
-							'string': 94,
-							'total': 99
-						}
-					}
-				]
-			},
-			{
-				key: 'secondTopKey',
-				types: {
-					'string': 4,
-					'total': 4
-				}
-			}
-		]
-		
-		var metaCol = Meta.findOne({name:FlowRouter.getParam('collectionName')});
-		
-		var fields = _.each(dummyData, function(val,key){
-			//
+		var metaCollection = Meta.findOne({
+			name: FlowRouter.getParam('collectionName')
 		});
-
-		return fields;
+		
+		var dummyData = {
+			'_id': { types: { string: 7, total: 7 }, depth: 1 },
+			'b': { types: { object: 5, total: 5 }, depth: 1 },
+			'b#1': { types: { boolean: 5, total: 5 }, depth: 2 },
+			'b#2': { types: { object: 5, total: 5 }, depth: 2 },
+			'b#2#c': { types: { boolean: 4, total: 4 }, depth: 3 },
+			'b#2#d': { types: { object: 5, total: 5 }, depth: 3 },
+			'b#2#d#1': { types: { boolean: 5, total: 5 }, depth: 4 },
+			'b#2#d#2': { types: { boolean: 4, total: 4 }, depth: 4 },
+			'name': { types: { string: 7, total: 7 }, depth: 1 },
+			'a': { types: { object: 5, total: 5 }, depth: 1 },
+			'a#1': { types: { boolean: 5, total: 5 }, depth: 2 },
+			'a#2': { types: { boolean: 3, total: 3 }, depth: 2 }
+		};
+		
+		var fieldNodes = _.map(_.keys(dummyData), function(fieldNodeKey){
+			var thisNode = dummyData[fieldNodeKey];
+			thisNode.key = _.last(fieldNodeKey.split('#'));
+			return thisNode;
+		});
+		
+		return fieldNodes;
 	}
 
 });
